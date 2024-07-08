e1ff565efe386b6643b359e56893376b865a9ab6
maliByatzes <malib2027@gmail.com>
2024-07-08 19:46:21 +0200

    implement the update user && password

--- backend/routes/user.routes.ts
+++ backend/routes/user.routes.ts
@@ -2,6 +2,9 @@ import { Hono } from "hono";
 import { protectRoute } from "../middleware/protectRoute";
 import type { Variables } from "./address.routes";
 import User from "../models/user.model";
+import { zValidator } from "@hono/zod-validator";
+import { updateUserPassword, updateUserSchema } from "../schemas/user.schema";
+import * as argon2 from "argon2";
 
 const userRoutes = new Hono<{ Variables: Variables }>();
 
@@ -21,8 +24,47 @@ userRoutes.get("/me", protectRoute, async (c) => {
   }
 });
 
-userRoutes.put("/update", protectRoute, async (c) => {
-  return c.text("Update user handler");
+userRoutes.put("/update-user", protectRoute, zValidator("json", updateUserSchema), async (c) => {
+  const userId = c.get("user")._id;
+  const userForm = c.req.valid("json");
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(userId, userForm, { new: true }).select("-password");
+    if (!updatedUser) {
+      return c.notFound();
+    }
+
+    return c.json(updatedUser, 200);
+  } catch (err: any) {
+    console.error(`Error in update user handler: ${err.message}`);
+    return c.json({ error: "Internal Server Error" }, 500);
+  }
+});
+
+userRoutes.put("/update-password", protectRoute, zValidator("json", updateUserPassword), async (c) => {
+  const userId = c.get("user")._id;
+  const userForm = c.req.valid("json");
+
+  try {
+    const existingUser = await User.findById(userId);
+    if (!existingUser) {
+      return c.notFound();
+    }
+
+    const isMatch = await argon2.verify(existingUser.password, userForm.oldPassword);
+    if (!isMatch) {
+      return c.json({ error: "Invalid old password" }, 401);
+    }
+
+    const hashedPassword = await argon2.hash(userForm.newPassword);
+    existingUser.password = hashedPassword;
+    await existingUser.save();
+
+    return c.json({ message: "success" }, 200);
+  } catch (err: any) {
+    console.error(`Error in update password handler: ${err.message}`);
+    return c.json({ error: "Internal Server Error" }, 500);
+  }
 });
 
 export default userRoutes;
--- backend/schemas/user.schema.ts
+++ backend/schemas/user.schema.ts
@@ -10,17 +10,28 @@ export const createUserSchema = z.object({
   phone: z.string().min(10),
   role: z.enum(ROLES),
 }).required()
-.refine((data) => Object.is(data.password, data.confirmPassword), {
-  message: "Passwords do not match",
-  path: ["confirmPassword"]
-});
+  .refine((data) => Object.is(data.password, data.confirmPassword), {
+    message: "Passwords do not match",
+    path: ["confirmPassword"]
+  });
 
 export const loginUserSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
 });
 
-/*
-export type CreateUserInput = z.infer<typeof createUserSchema>['body'];
-export type LoginUserInput = z.infer<typeof loginUserSchema>['body'];
-*/
+export const updateUserSchema = z.object({
+  name: z.string().max(101),
+  email: z.string().email(),
+  phone: z.string().min(10),
+}).partial();
+
+export const updateUserPassword = z.object({
+  oldPassword: z.string().min(8),
+  newPassword: z.string().min(8),
+  confirmPassword: z.string()
+}).required()
+  .refine((data) => Object.is(data.newPassword, data.confirmPassword), {
+    message: "Passwords do not match",
+    path: ["confirmPassword"]
+  });
