712a0c7f1ff8d89615b026cc6401d5e66ea24f22
maliByatzes <malib2027@gmail.com>
2024-07-09 02:16:53 +0200

    scaffold restaurant routes

--- backend/routes/restaurant.routes.ts
+++ backend/routes/restaurant.routes.ts
@@ -1,5 +1,26 @@
 import { Hono } from "hono";
+import { protectRoute, requireAdmin } from "../middleware/protectRoute";
 
 const restaurantRoutes = new Hono();
+
+restaurantRoutes.post("/", protectRoute, requireAdmin, async (c) => {
+  return c.text("create restaurant route");
+});
+
+restaurantRoutes.get("/", async (c) => {
+  return c.text("get all restaurants route");
+});
+
+restaurantRoutes.get("/:id", async (c) => {
+  return c.text("get one restaurant route");
+});
+
+restaurantRoutes.put("/:id", protectRoute, requireAdmin, async (c) => {
+  return c.text("update one restaurant route");
+});
+
+restaurantRoutes.delete("/:id", protectRoute, requireAdmin, async (c) => {
+  return c.text("delete one restaurant route");
+});
 
 export default restaurantRoutes;
