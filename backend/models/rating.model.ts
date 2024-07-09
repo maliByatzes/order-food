cd7802ec0dd458b312335df13d43f906a137e69e
maliByatzes <malib2027@gmail.com>
2024-07-09 20:24:19 +0200

    define rating model and schema

---
+++ backend/models/rating.model.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+export interface IRating {
+  _id: mongoose.Types.ObjectId,
+  userId: mongoose.Types.ObjectId,
+  restauarantId: mongoose.Types.ObjectId,
+  rating: number,
+  createdAt: Date,
+  updatedAt: Date
+};
+
+const ratingSchema = new mongoose.Schema<IRating>({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  restauarantId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Restaurant",
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true
+  }
+}, { timestamps: true });
+
+const Rating = mongoose.model<IRating>("Rating", ratingSchema);
+
+export default Rating;
