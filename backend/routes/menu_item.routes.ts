91f02795192bf68595f1f7086811512f48a04317
maliByatzes <malib2027@gmail.com>
2024-07-09 10:10:53 +0200

    scaffold menu item routes

--- backend/routes/menu_item.routes.ts
+++ backend/routes/menu_item.routes.ts
@@ -1,5 +1,30 @@
 import { Hono } from "hono";
+import { protectRoute, requireAdmin } from "../middleware/protectRoute";
 
 const menuItemRoutes = new Hono();
+
+menuItemRoutes.post(
+  "/",
+  protectRoute,
+  requireAdmin,
+  async (c) => {
+    return c.text("Create menu item route");
+  });
+
+menuItemRoutes.put(
+  "/:id",
+  protectRoute,
+  requireAdmin,
+  async (c) => {
+    return c.text("update one menu item route");
+  });
+
+menuItemRoutes.delete(
+  "/:id",
+  protectRoute,
+  requireAdmin,
+  async (c) => {
+    return c.text("delete one menu item route");
+  });
 
 export default menuItemRoutes;
