4f4fbe482a9ff0f69d021af7ad135adbeee84a2e
maliByatzes <malib2027@gmail.com>
2024-07-09 21:04:14 +0200

    implement more rating route

--- backend/routes/rating.routes.ts
+++ backend/routes/rating.routes.ts
@@ -18,6 +18,10 @@ ratingRoutes.post(
     const restaurantId = c.req.param("restaurantId");
 
     try {
+      if (restaurantId.length !== 24) {
+        return c.json({ error: "Invalid restaurant id" }, 400);
+      }
+
       const restaurant = await Restaurant.findById(restaurantId);
       if (!restaurant) {
         return c.notFound();
@@ -45,5 +49,62 @@ ratingRoutes.post(
       return c.json({ error: "Internal Server Error" }, 500);
     }
   });
+
+ratingRoutes.get("/average/:restaurantId", async (c) => {
+  const restaurantId = c.req.param("restaurantId");
+
+  try {
+    if (restaurantId.length !== 24) {
+      return c.json({ error: "Invalid restaurant id" }, 400);
+    }
+
+    const restaurant = await Restaurant.findById(restaurantId);
+    if (!restaurant) {
+      return c.notFound();
+    }
+
+    const ratings = await Rating.find({ restaurantId: restaurant._id });
+    const averageRating = ratings.reduce((accumulator, rating) => accumulator + rating.rating, 0) / ratings.length;
+
+
+    return c.json({ averageRating }, 200);
+  } catch (err: any) {
+    console.error(`Error in get average rating handler: ${err.message}`);
+    return c.json({ error: "Internal Server Error" }, 500);
+  }
+});
+
+ratingRoutes.get("/", protectRoute, async (c) => {
+  const user = c.get("user");
+
+  try {
+    const ratings = await Rating.find({ userId: user._id }).populate("restaurantId");
+    return c.json(ratings, 200);
+  } catch (err: any) {
+    console.error(`Error in get all user's ratings handler: ${err.message}`);
+    return c.json({ error: "Internal Server Error" }, 500);
+  }
+});
+
+ratingRoutes.get("/:id", protectRoute, async (c) => {
+  const user = c.get("user");
+  const ratingId = c.req.param("id");
+
+  try {
+    if (ratingId.length !== 24) {
+      return c.json({ error: "Invalid rating id" }, 400);
+    }
+
+    const rating = await Rating.findOne({ _id: ratingId, userId: user._id }).populate("restaurantId");
+    if (!rating) {
+      return c.notFound();
+    }
+
+    return c.json(rating, 200);
+  } catch (err: any) {
+    console.error(`Error in get all user's ratings handler: ${err.message}`);
+    return c.json({ error: "Internal Server Error" }, 500);
+  }
+});
 
 export default ratingRoutes;
